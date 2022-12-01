46493528ebf13a962c2a5d23785b40db8cddb3fe
hussy.levente <hussy.levente@28-08>
2022-12-01 15:26:15 +0100

    Add project files.

---
+++ hussy_levente_beadando_20221201/Program.cs
@@ -0,0 +1,19 @@
+﻿Random rnd = new Random();
+
+string[] ValasztasokTMB = "kő|papír|olló|gyík|speck".Split('|');
+
+
+int FelhPontjai = 0;
+int GepPontjai = 0;
+
+while (FelhPontjai - GepPontjai != 3 || GepPontjai - FelhPontjai != 3)
+{
+    string GepFelel = ValasztasokTMB[rnd.Next(ValasztasokTMB.Length)];
+    Console.Write($"Add meg hogy mit akarsz mutatni: ");
+    string FelhMutat = Console.ReadLine();
+    Console.WriteLine($"Amit a gép mutatott: {GepFelel}\n");
+    if (FelhMutat == GepFelel)
+    {
+        Console.WriteLine("Döntetlen.\n");
+    }
+}
