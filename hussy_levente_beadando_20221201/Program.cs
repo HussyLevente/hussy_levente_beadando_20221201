6394cfcbb8bd392a10bad31c397fd50a46f62c5e
Hussy Levente <hussy.l.hunor@gmail.com>
2022-12-03 10:27:29 +0100

    Elkészült munka

--- hussy_levente_beadando_20221201/Program.cs
+++ hussy_levente_beadando_20221201/Program.cs
@@ -1,4 +1,6 @@
-﻿Random rnd = new Random();
+﻿using System.Runtime.InteropServices;
+
+Random rnd = new Random();
 
 string[] ValasztasokTMB = "kő|papír|olló|gyík|speck".Split('|');
 
@@ -6,14 +8,67 @@ string[] ValasztasokTMB = "kő|papír|olló|gyík|speck".Split('|');
 int FelhPontjai = 0;
 int GepPontjai = 0;
 
-while (FelhPontjai - GepPontjai != 3 || GepPontjai - FelhPontjai != 3)
+
+while (FelhPontjai - GepPontjai != 3 && GepPontjai - FelhPontjai != 3)
 {
+    bool Szerzett = false;
     string GepFelel = ValasztasokTMB[rnd.Next(ValasztasokTMB.Length)];
     Console.Write($"Add meg hogy mit akarsz mutatni: ");
     string FelhMutat = Console.ReadLine();
     Console.WriteLine($"Amit a gép mutatott: {GepFelel}\n");
     if (FelhMutat == GepFelel)
     {
-        Console.WriteLine("Döntetlen.\n");
+        Console.WriteLine("Döntetlen, senki nem kap pontot.\n");
+        Szerzett = true;
     }
+    if (FelhMutat == "kő" && GepFelel == "gyík" || FelhMutat == "kő" && GepFelel == "olló")
+    {
+        Console.WriteLine("A felhasználó szerzett egy pontot!\n");
+        FelhPontjai += 1;
+        Szerzett = true;
+    }
+
+    if (FelhMutat == "papír" && GepFelel == "spock" || FelhMutat == "papír" && GepFelel == "kő")
+    {
+        Console.WriteLine("A felhasználó szerzett egy pontot!\n");
+        FelhPontjai += 1;
+        Szerzett = true;
+    }
+
+    if (FelhMutat == "olló" && GepFelel == "papír" || FelhMutat == "olló" && GepFelel == "gyík")
+    {
+        Console.WriteLine("A felhasználó szerzett egy pontot!\n");
+        FelhPontjai += 1;
+        Szerzett = true;
+    }
+
+    if (FelhMutat == "gyík" && GepFelel == "spock" || FelhMutat == "gyík" && GepFelel == "papír")
+    {
+        Console.WriteLine("A felhasználó szerzett egy pontot!\n");
+        FelhPontjai += 1;
+        Szerzett = true;
+    }
+
+    if (FelhMutat == "spock" && GepFelel == "olló" || FelhMutat == "spock" && GepFelel == "kő")
+    {
+        Console.WriteLine("A felhasználó szerzett egy pontot!\n");
+        FelhPontjai += 1;
+        Szerzett = true;
+    }
+
+    if (Szerzett != true)
+    {
+        Console.WriteLine("A gép szerzett egy pontot\n");
+        GepPontjai += 1;
+    }
+}
+
+Console.WriteLine($"Felhasználó pontjai: {FelhPontjai}\nGép pontjai: {GepPontjai}");
+if (FelhPontjai > GepPontjai)
+{
+    Console.WriteLine("A felhasználó nyert! :D");
+}
+else
+{
+    Console.WriteLine("A gép nyert! :(");
 }
